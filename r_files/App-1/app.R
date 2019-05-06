e964930fc5aecf166a3b1b3856e0a0ad3d0d6dd9
buehlere <buehle_e1@denison.edu>
2019-05-06 14:58:44 -0400

    finish app

--- r_files/App-1/app.R
+++ r_files/App-1/app.R
@@ -1,38 +1,74 @@
 
 
 library(shiny)
+library(caret)
 library(readr)
 library(ggplot2)
-library(caret)
-setwd("C:/Users/buehl/Documents")
+
 load("RandomForestDia.rda")
 load("RandomForestSys.rda")
-mse <- read.csv("mse.csv")
-
+mse <- read_csv("mse.csv")
 
 ui <- fluidPage(
   h1("Please Enter Patient Information:"),
   sliderInput(inputId = "Age",label = "Enter Age (yrs.)", 
-              value = 10, min = 1, max = 100),
+              value = 1, min = 1, max = 100),
   
   sliderInput(inputId = "Weight",label = "Enter Weight (kg)", 
               value = 1, min = 1, max = 300), 
   
-  sliderInput(inputId = "BMI",label = "Enter BMI (kg/m^2)", 
+  sliderInput(inputId = "Waist",label = "Enter Waist Circumference (cm)", 
               value = 1, min = 1, max = 300), 
   
-  sliderInput(inputId = "BMI",label = "Enter Height (cm)", 
+  sliderInput(inputId = "Cholesterol",label = "Enter Cholesterol (mg/dl)", 
+              value = 1, min = 1, max = 500), 
+  
+  sliderInput(inputId = "LDL",label = "Enter LDL Cholesterol (mg/dl)", 
               value = 1, min = 1, max = 300), 
+  
+  sliderInput(inputId = "HDL",label = "Enter HDL Cholesterol (mg/dl)", 
+              value = 1, min = 1, max = 300), 
+  
+  sliderInput(inputId = "BMI",label = "Enter BMI (kg/m^2)", 
+              value = 1, min = 1, max = 100), 
+  
+  sliderInput(inputId = "ArmC",label = "Enter Upper Arm Circumference (cm)", 
+              value = 1, min = 1, max = 100), 
+  
+  sliderInput(inputId = "Abdominal",label = "Enter Abdominal Circumference (cm)", 
+              value = 1, min = 1, max = 100), 
+  
+  sliderInput(inputId = "Height",label = "Enter Height (cm)", 
+              value = 1, min = 1, max = 300), 
+  
+  sliderInput(inputId = "LowerA",label = "Enter Arm Circumference (cm)", 
+              value = 1, min = 1, max = 100),
+  
+  sliderInput(inputId = "LegC",label = "Enter Leg Cirumference (cm)", 
+              value = 1, min = 1, max = 100),
+  
+  sliderInput(inputId = "glucose",label = "Enter Glucose (mg/dL)", 
+              value = 1, min = 1, max = 500),
   
   selectInput(inputId = "Gender", label = "Enter Gender", choices = c("Male","Female")), 
+  
+  selectInput(inputId = "Race", label = "Enter Race", choices = c("White","Black","Asian","Hispanic")),
+  
+  selectInput(inputId = "Diabetes", label = "Diabetes?", choices = c("Yes", "No")),
+  
+  selectInput(inputId = "Hypertension", label = "Hypertensive?", choices = c("Yes","No")), 
+  
+  selectInput(inputId = "Hypertension2", label = "Hypertensive Medication?", choices = c("Yes","No")),
+  
+  
   
   h1("Please Enter Observed Blood Pressure:"),
   
   sliderInput(inputId = "Diastolic",label = "Enter Observed Diastolic Blood Pressure", 
-              value = 5, min = 1, max = 300), 
+              value = 50, min = 1, max = 200), 
   
   sliderInput(inputId = "Systolic",label = "Enter Observed Systolic Blood Pressure Blood Pressure", 
-              value = 300, min = 1, max = 300), 
+              value = 50, min = 1, max = 200), 
   
   h1("The Reading Is:"),
   
@@ -42,41 +78,70 @@ ui <- fluidPage(
 
 server <- function(input,output) {
   
-  output$status <- renderPrint({
+  statement <- reactive({
     ###sys 
-    linear = lm(sys_mean~ridageyr+bmxwt,lin_frame_clean)
-    prediction_frame <- data.frame(ridageyr = input$Age,bmxwt = input$Weight)
-    prediction_sys <- predict(linear, prediction_frame)
+    prediction_frame <- data.frame(ridageyr = input$Age, 
+                                   bmxwt = input$Weight, 
+                                   bmxwaist = input$Waist, 
+                                   lbxtc = input$Cholesterol,
+                                   lbdldl = input$LDL,
+                                   lbdhdd = input$HDL,
+                                   bmxbmi = input$BMI,
+                                   bmxarmc = input$ArmC,
+                                   bmxsad1 = input$Abdominal,
+                                   bmxht = input$Height,
+                                   bmxarml = input$LowerA,
+                                   bmxleg = input$LegC, 
+                                   lbxglu = input$glucose, 
+                                   riagendr = input$Gender,
+                                   bpq020 = input$Hypertension,
+                                   bpq040a = input$Hypertension2,
+                                   diq010 = input$Diabetes, 
+                                   white = 0, 
+                                   black = 0, 
+                                   asian = 0,
+                                   hispanic = 0)
     
+    ###Medical Dummies 
+    prediction_frame$riagendr <- ifelse(prediction_frame$riagendr == "Male",1,0)
+    prediction_frame$bpq020 <- ifelse(prediction_frame$bpq020 == "Yes",1,0)
+    prediction_frame$bpq040a <- ifelse(prediction_frame$bpq040a == "Yes",1,0)
+    prediction_frame$diq010 <- ifelse(prediction_frame$diq010 == "Yes",1,0)
     
-    ###dia 
-    linear = lm(dia_mean~ridageyr+bmxwt,lin_frame_clean)
-    prediction_frame <- data.frame(ridageyr = input$Age,bmxwt = input$Weight)
-    prediction_dia <- predict(linear, newdata = prediction_frame)
+    ###race Dummies 
+    prediction_frame$white <- ifelse(input$Race == "White",1,0)
+    prediction_frame$black <- ifelse(input$Race == "Black",1,0)
+    prediction_frame$asian <- ifelse(input$Race == "Asian",1,0)
+    prediction_frame$hispanic <- ifelse(input$Race == "Hispanic",1,0)
     
-    ##distnace
-    prediction <- data.frame(preds = prediction_sys,predd = prediction_dia)
-    observed <- data.frame(systolic = input$Systolic,diastolic = input$Diastolic)
-    a <- cbind(observed$systolic, observed$diastolic) 
-    b <- cbind(prediction$preds, prediction$predd)
+    ###making prediction
+    rf_dia_pred <- predict(rf_dia_tree, newdata = prediction_frame)
+    rf_sys_pred <- predict(rf_sys_tree, newdata = prediction_frame)
     
+    ###frames for distance calculation 
+    predicted <- cbind(rf_dia_pred,rf_sys_pred)
+    observed <- cbind(input$Diastolic,input$Systolic)
     
-    if(is.na(distance))
+    ###information for the covariance matrix 
+    mal_pred <- cbind(mse$rf_pred_dia,mse$rf_pred_sys)
+    mal_true <- cbind(mse$true_dia,mse$true_sys)
+    
+    ###distance calculation 
+    distance <- ((observed - predicted) %*% solve(cov(mal_pred,mal_true)) %*% t((observed-predicted)))
+    if(distance > 149)
     {
-      print("valid")
+      statement <- "MEASUREMENT ERROR"
     } else 
     {
-      if(distance > 149)
-      {
-        print("not valid")
-      }
-      else
-      {
-        print("valid")
-      } 
+      statement <- "VALID"
     }
-
+    statement
     
+  })
+  output$status <- renderPrint({
+
+    print(statement()) 
+
   }) 
   
 } 
